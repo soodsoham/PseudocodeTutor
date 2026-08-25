34f8b9e27a1ca4c7c07ebd065a351561cc42d606
Soham Sood <292689917+soodsoham@users.noreply.github.com>
2026-08-25 19:32:16 +0530

    Match pseudocode text spacing to code gutter

--- frontend/src/components/editor/PseudocodePanel.tsx
+++ frontend/src/components/editor/PseudocodePanel.tsx
@@ -283,7 +283,7 @@ function PseudocodePanel() {
           lineHeight: '1.6',
           tabSize: 2,
           whiteSpace: 'pre',
-          paddingLeft: '56px',
+          paddingLeft: '8px',
           paddingTop: 0,
           paddingBottom: 0,
           overflowX: 'auto',
